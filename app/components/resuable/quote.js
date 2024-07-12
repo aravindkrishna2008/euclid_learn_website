7000d75e3382ee4f58090ba84fc4e2ba1a11e80a
aravindkrishna2008 <86750995+aravindkrishna2008@users.noreply.github.com>
2024-07-12 13:05:35 -0700

    build fix

--- app/components/resuable/quote.js
+++ app/components/resuable/quote.js
@@ -23,7 +23,7 @@ const Quote = () => {
           Our Happy Students
         </h1>
         <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#25272C] mt-[1.25vw] w-[80vw] sm:w-[32vw] mx-auto">
-          Don&apos;t just take our word for it. Hear what our students have to 
+          Don&apos;t just take our word for it. Hear what our students have to
           say about their experience
         </p>
       </motion.div>
@@ -42,7 +42,15 @@ const Quote = () => {
               className="sm:w-[2.2vw] sm:h-[2.2vw] w-[5vw] h-[5vw]"
             />
             <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#383A42] mt-[4vw] sm:mt-[1.25vw] w-[80vw] sm:w-[23vw] mx-auto">
-            We are grateful to Woojin for creating an incredibly positive learning environment for my daughter throughout the year. My daughter showed significant improvement in both test scores and grades in a short period of time. In essence, these classes identify the exact questions that appear on assessments and provide solutions in a way that is simple for the student to comprehend. My daughter highly recommends these classes to anyone looking to boost their academic performance and achieve better grades in school.
+              We are grateful to Woojin for creating an incredibly positive
+              learning environment for my daughter throughout the year. My
+              daughter showed significant improvement in both test scores and
+              grades in a short period of time. In essence, these classes
+              identify the exact questions that appear on assessments and
+              provide solutions in a way that is simple for the student to
+              comprehend. My daughter highly recommends these classes to anyone
+              looking to boost their academic performance and achieve better
+              grades in school.
             </p>
             <div className="flex flex-row mt-[4vw] sm:mt-[1.25vw]">
               <Image
@@ -53,8 +61,7 @@ const Quote = () => {
               />
               <div className="flex flex-col ml-[1.25vw] justify-center">
                 <h1 className="sm:text-[1.4vw] text-[5vw] font-semibold text-[#5B616E] tracking-[-0.014vw] leading-[130%]">
-                Anjali 
-
+                  Anjali
                 </h1>
                 <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#8E95A2]">
                   Parent
@@ -70,7 +77,19 @@ const Quote = () => {
               className="sm:w-[2.2vw] sm:h-[2.2vw] w-[5vw] h-[5vw]"
             />
             <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#383A42] mt-[4vw] sm:mt-[1.25vw] w-[80vw] sm:w-[23vw] mx-auto">
-            Working with Abhinav was an amazing experience. Thanks to him and the team, I&apos;ve developed a solid strategy for navigating high school. Under Abhinav&apos;s guidance, I achieved a significant milestone by publishing my research paper in a respected journal (IEEE). I was suprised by how his advice not only boosted my grades but also sharpened my time management skills and increased my confidence in my own abilities. <Link href="https://assets-eu.researchsquare.com/files/rs-4294555/v2_covered_4399fd54-d645-4560-9f30-7b572780c2c3.pdf?c=1717434129" className="text-[#2a55ff] hover:underline">Link to my research paper</Link>
+              Working with Abhinav was an amazing experience. Thanks to him and
+              the team, I&apos;ve developed a solid strategy for navigating high
+              school. Under Abhinav&apos;s guidance, I achieved a significant
+              milestone by publishing my research paper in a respected journal
+              (IEEE). I was suprised by how his advice not only boosted my
+              grades but also sharpened my time management skills and increased
+              my confidence in my own abilities.{" "}
+              <Link
+                href="https://assets-eu.researchsquare.com/files/rs-4294555/v2_covered_4399fd54-d645-4560-9f30-7b572780c2c3.pdf?c=1717434129"
+                className="text-[#2a55ff] hover:underline"
+              >
+                Link to my research paper
+              </Link>
             </p>
             <div className="flex flex-row mt-[4vw] sm:mt-[1.25vw]">
               <Image
@@ -81,10 +100,10 @@ const Quote = () => {
               />
               <div className="flex flex-col ml-[1.25vw] justify-center">
                 <h1 className="sm:text-[1.4vw] text-[5vw] font-semibold text-[#5B616E] tracking-[-0.014vw] leading-[130%]">
-              Shanker 
+                  Shanker
                 </h1>
                 <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#8E95A2]">
-                San Jose, Ca
+                  San Jose, Ca
                 </p>
               </div>
             </div>
@@ -104,7 +123,15 @@ const Quote = () => {
               className="sm:w-[2.2vw] sm:h-[2.2vw] w-[5vw] h-[5vw]"
             />
             <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#383A42] mt-[4vw] sm:mt-[1.25vw] w-[80vw] sm:w-[23vw] mx-auto">
-            Abhinav's expertise in teaching competitive programming is unmatched. What sets Abhinav apart is his commitment to his students and attention to detail —he consistently went above and beyond to assist with practice problems, making sure I thorougly understood every concept, even outside of our sessions. Abhinav is also very approachable and creates a comfortable learning environment where I never hesitated to ask questions or seek clarification. I highly recommend his services without reservation. 
+              Abhinav&apos;s expertise in teaching competitive programming is
+              unmatched. What sets Abhinav apart is his commitment to his
+              students and attention to detail —he consistently went above and
+              beyond to assist with practice problems, making sure I thorougly
+              understood every concept, even outside of our sessions. Abhinav is
+              also very approachable and creates a comfortable learning
+              environment where I never hesitated to ask questions or seek
+              clarification. I highly recommend his services without
+              reservation.
             </p>
             <div className="flex flex-row mt-[4vw] sm:mt-[1.25vw]">
               <Image
@@ -115,10 +142,10 @@ const Quote = () => {
               />
               <div className="flex flex-col ml-[1.25vw] justify-center">
                 <h1 className="sm:text-[1.4vw] text-[5vw] font-semibold text-[#5B616E] tracking-[-0.014vw] leading-[130%]">
-                Anish                </h1>
+                  Anish{" "}
+                </h1>
                 <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#8E95A2]">
-                Student 
-
+                  Student
                 </p>
               </div>
             </div>
@@ -131,7 +158,14 @@ const Quote = () => {
               className="sm:w-[2.2vw] sm:h-[2.2vw] w-[5vw] h-[5vw]"
             />
             <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#383A42] mt-[4vw] sm:mt-[1.25vw] w-[80vw] sm:w-[23vw] mx-auto">
-            Anirudh literally saved my academic career! I felt so lost and overwhelmed in my classes when April rolled around so I was convinced I would do terrible on the AP Exams. When a friend recommended Anirudh, I decided to give him a shot, and it turns out hes an AMAZING teacher. I still can&apos;t believe I scored a 5 on APUSH, Calculus AB and a 4 on AP Language. Please use him if your in a similar spot I seriously can&apos;t recommend this guy enough.
+              Anirudh literally saved my academic career! I felt so lost and
+              overwhelmed in my classes when April rolled around so I was
+              convinced I would do terrible on the AP Exams. When a friend
+              recommended Anirudh, I decided to give him a shot, and it turns
+              out hes an AMAZING teacher. I still can&apos;t believe I scored a
+              5 on APUSH, Calculus AB and a 4 on AP Language. Please use him if
+              your in a similar spot I seriously can&apos;t recommend this guy
+              enough.
             </p>
             <div className="flex flex-row mt-[4vw] sm:mt-[1.25vw]">
               <Image
@@ -142,10 +176,11 @@ const Quote = () => {
               />
               <div className="flex flex-col ml-[1.25vw] justify-center">
                 <h1 className="sm:text-[1.4vw] text-[5vw] font-semibold text-[#5B616E] tracking-[-0.014vw] leading-[130%]">
-                Stacy
+                  Stacy
                 </h1>
                 <p className="sm:text-[1.25vw] text-[4vw] font-light text-[#8E95A2]">
-                Wisconsin                </p>
+                  Wisconsin{" "}
+                </p>
               </div>
             </div>
           </div>
