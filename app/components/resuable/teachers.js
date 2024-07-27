bec0b6770a034810efc4dfffbcab283f68d65ce4
Ashish R <107366609+Username1unavailable@users.noreply.github.com>
2024-07-27 16:28:43 -0700

    we

--- app/components/resuable/teachers.js
+++ app/components/resuable/teachers.js
@@ -22,27 +22,24 @@ const Teachers = ({ teacherRef }) => {
         prestigious awards, high standardized test scores, and extensive
         teaching experience.
       </motion.p>
-      <div className="flex flex-col sm:flex-row gap-[4vw] sm:gap-[1.25vw] mt-[5vw]">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-[4vw] sm:gap-[1.25vw] mt-[5vw]">
         <motion.div
           initial={{ scale: 0.8, opacity: 0 }}
           whileInView={{ scale: 1, opacity: 1 }}
           transition={{ duration: 1 }}
           className="flex flex-col gap-[1.875vw] justify-center items-center text-center"
         >
-          <Image width={10000} height={10000} 
-          
-          className="rounded-[1.6vw] h-[23vw] w-[750vw]"
-
-          src="/team/default.webp" />
+          <Image width={10000} height={10000} src="/team/Anirudh.png" />
           <div>
             <p className="text-[#5B616E] text-[5vw] sm:text-[1.4vw] font-semibold tracking-normal">
-            Shivang Tiwari            
+              Anirudh Shyam
             </p>
             <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
-            ICPC 2023 World Finalist             
+              4 Yrs of Mentoring Experience
             </p>
             <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
-            EE at IIT Bombay            </p>
+              1580 SAT Score
+            </p>
           </div>
         </motion.div>
         <motion.div
@@ -57,11 +54,10 @@ const Teachers = ({ teacherRef }) => {
               Woojin Yoon
             </p>
             <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
-            240 USAMO Index
-
+              240 USAMO Index
             </p>
             <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
-            1570 SAT Score 
+              1570 SAT Score
             </p>
           </div>
         </motion.div>
@@ -84,7 +80,55 @@ const Teachers = ({ teacherRef }) => {
             <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
               1580 SAT Score
             </p>
-            
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="flex flex-col gap-[1.875vw] justify-center items-center text-center"
+        >
+          <Image
+            width={10000}
+            height={10000}
+            className="rounded-[1.6vw] h-[23vw] w-[750vw]"
+            src="/team/default.webp"
+          />
+          <div>
+            <p className="text-[#5B616E] text-[5vw] sm:text-[1.4vw] font-semibold tracking-normal">
+              Shivang Tiwari
+            </p>
+            <p className="text-[#196aff] font-light text-[3vw] sm:text-[1vw] underline">
+            <a href="https://icpc.global/ICPCID/TA70T9WDEW5N" target="_blank" rel="noopener noreferrer">
+            ICPC 2023 World Finalist              </a>
+            </p>
+            <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
+              EE at IIT Bombay
+            </p>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="flex flex-col gap-[1.875vw] justify-center items-center text-center"
+        >
+          <Image
+            width={10000}
+            height={10000}
+            className="rounded-[1.6vw] h-[23vw] w-[750vw]"
+            src="/team/default.webp"
+          />
+          <div>
+            <p className="text-[#5B616E] text-[5vw] sm:text-[1.4vw] font-semibold tracking-normal">
+              Kunal Kundwani
+            </p>
+            <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
+              ICPC 2023 World Finalist
+            </p>
+            <p className="text-[#8E95A2] font-light text-[3vw] sm:text-[1vw]">
+              EE at IIT Bombay
+            </p>
           </div>
         </motion.div>
       </div>
