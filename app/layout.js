b3f1dfdd2387ddd3c58c74f287f5887a86ea7228
Ashish R <107366609+Username1unavailable@users.noreply.github.com>
2024-12-29 19:25:59 -0800

    we

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,12 @@ export const metadata = {
   title: "Euclid Learn",
   description: "Score higher on the SAT, AP Exams, AMC, USACO, Codeforces, and Tech Interviews with our expert guidance, coaching, and tutoring. ",
 };
-
+//wewe
 export default function RootLayout({ children }) {
   return (
     <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang="en">
       <link rel="icon" href="/icons/smiley/smile_blue.svg" sizes="any" />
+      <head />
       <body>{children}</body>
     </html>
   );
