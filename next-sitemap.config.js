0b4830926e0da4ca98a74fd6a7ccafc58bc9d0be
Ashish R <107366609+Username1unavailable@users.noreply.github.com>
2024-12-29 23:10:15 -0800

    er

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.euclidlearn.com', // Replace with your website's URL
+    generateRobotsTxt: true, // Automatically generate a robots.txt file
+    sitemapSize: 5000, // Optional, split sitemaps if you have too many URLs
+  };
+  
+  module.exports = config;
+  
